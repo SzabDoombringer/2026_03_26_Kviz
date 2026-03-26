4119aa0bd30687b865851235d78e4db0f61ba930
Szabi <bodor.szabolcs@diak.szbi-pg.hu>
2026-03-26 12:28:40 +0100

    c

---
+++ value.js
@@ -0,0 +1,9 @@
+
+function szamol(){
+    console.log("Helló");
+    let szelesseg = document.getElementById("szelesseg").value
+    let magassag = document.getElementById("magassag").value
+}
+
+console.log(magassag)
+console.log(szelesseg)
